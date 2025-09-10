7b2f8578dd61e65398fb49befe3d40197689a603
masonkulikowski <masonskulikowski@gmail.com>
2025-09-10 16:47:14 -0400

    assignment styles

--- apps/web/app/assignment/page.tsx
+++ apps/web/app/assignment/page.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import styles from "./assignment.module.css";
+
 
 export default function Assignment() {
   return (
-    <main style={{ padding: "1rem" }}>
+    <main className={styles.main}>
       <Link href="/course">&lt; Back to Course</Link>
-      <h1 style={{ marginTop: "1rem" }}>NextJS Learning</h1>
-      <p>due 9/10</p>
-      <p style={{ marginTop: "1rem" }}>
+      <h1 className={styles.title}>NextJS Learning</h1>
+      <p className={styles.dueDate}>due 9/10</p>
+      <p className={styles.description}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
         habitant morbi tristique senectus et netus.
       </p>
-      <div style={{ marginTop: "1rem" }}>
+      <div className={styles.upload}>
         <input type="file" />
       </div>
-      <button style={{ marginTop: "1rem" }}>Submit</button>
-      <p style={{ marginTop: "1rem" }}>
+      <button className={styles.button}>Submit</button>
+      <p className={styles.viewLink}>
         <Link href="/submission">View submissions</Link>
       </p>
     </main>
